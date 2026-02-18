eb7f0eb27e946013378b592f73bcdb9136bad30a
MISTU293-art <mistu14112006@gmail.com>
2026-02-18 18:35:57 +0530

    DONE

---
+++ calculator.py
@@ -0,0 +1,39 @@
+print("Calculator")
+
+
+
+def cal():
+    while True:
+        try:
+            num_1 = float(input("Enter First Number:"))
+            num_2=float(input("Enter Second Number:"))
+        except:
+            print("Please Enter Valid Numbers!!")
+            continue    
+        choices=['+','-','*','/','exit']
+        print(choices)
+        user_choice=input("Enter Your Choice:")
+        if(user_choice==choices[0]):
+            addition=num_1+num_2
+            print("Addition Is:",addition)
+        elif(user_choice==choices[1]):
+            subtraction=num_1-num_2
+            print("Subtraction:",subtraction)
+        elif(user_choice==choices[2]):
+            multiplication=num_1*num_2
+            print("Multiplication:",multiplication) 
+        elif(user_choice==choices[3]):
+            if(num_2==0):
+                print("Can't divide by zero")
+            else:
+                division=num_1/num_2
+                print("Division:",division)
+        elif(user_choice==choices[4]):
+            print("Calculator Closed")
+            break
+        else:
+            print("invalid Choice")                   
+
+        
+
+cal()
